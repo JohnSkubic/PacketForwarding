947f625e5afe021c7cbc8b5b7cbb048c82e4a2d7
Nicholas Pfister <npfister@purdue.edu>
2017-04-25 22:33:40 -0400

    nearly done with hash functions, scalable search fx & build fx (lots to do) left

--- source/scalable_tables/scalable_tables.c
+++ source/scalable_tables/scalable_tables.c
@@ -2,8 +2,8 @@
  *
  *  Created By: Nick Pfister
  *
- *  Description:
- *
+ *  Description: "Scalable High Speed Routing Lookups"
+ *               part of implementation of http://conferences.sigcomm.org/sigcomm/1997/papers/p182.pdf
  */
 
 #include <stdio.h>
@@ -11,7 +11,6 @@
 #include <string.h>
 #include "utility.h"
 #include "scalable_tables.h"
-//#include "np_hashtables.h"
 
 int main (int argc, char *argv[]) {
   route_table_entry_t *table;
@@ -65,11 +64,11 @@ int main (int argc, char *argv[]) {
   }
 
   // 2nd pass -- scalable table = ropes and hash tables
-  //*****small_table_t *s_table = NULL;
-  //if((*****s_table = *****build_small_table(table, num_entries)) == NULL) {
-    //printf("Error: Could not build scalable table\n");
-    //return EXIT_FAILURE;
-  //}
+  htable_t ** scalable_table = NULL;
+  if((scalable_table = build_scalable_table(table, num_entries)) == NULL) {
+    printf("Error: Could not build scalable table\n");
+    return EXIT_FAILURE;
+  }
   //no longer need "1st pass trie" after scalable table is built
   destroy_trie_table(trie);
 
@@ -84,6 +83,120 @@ int main (int argc, char *argv[]) {
 
   return EXIT_SUCCESS;
 }
+
+// ********** BEGIN SCALABLE FUNCTIONS **********
+
+htable_t ** build_scalable_table(route_table_entry_t * table, int num_entries){
+	htable_t ** scalable_table;
+	scalable_table = init_scalable_table((uint32_t)32);//init array of hash table pointers, IPv4 w/ notions to expandability to IPv6
+
+
+
+	return scalable_table;
+}
+
+htable_t ** init_scalable_table(uint32_t num_levels){
+	htable_t ** scalable_table;
+	scalable_table = malloc(num_levels*sizeof(htable_t *));
+
+	return scalable_table;
+}
+
+// ********** END SCALABLE FUNCTIONS **********
+
+// ********** BEGIN HASHTABLE FUNCTIONS **********
+
+//valid prefix_levels 1 to 32 inclusive 
+htable_t * htable_create(uint32_t prefix_level){
+	htable_t * htable;
+	uint32_t num_buckets,i;
+	uint32_t temp_key,shamt,shamt_mask;
+
+	num_buckets = (1 << prefix_level);//2 to power --> prefix_level
+	num_buckets = ( num_buckets < MAX_BUCKETS) ? num_buckets : MAX_BUCKETS ;//handles 0 returned by 2^32 if MAX_BUCKETS is that big
+
+	//for hash function, shamt would otherwise need to be computed on each call
+	//compute once on creation, not per call to htable_hash()
+	shamt_mask = 0;
+	temp_key = num_buckets;//num_buckets is always a power of 2 (requirement)
+	while(temp_key >>= 1) ++shamt_mask;//shamt=log2(num_buckets),2^shamt=num_buckets
+	shamt = 32 - shamt_mask;//prefix lookups implies interest in MSB bits
+
+	//initialize hash table
+	htable = malloc(sizeof(htable_t));
+	htable->level = prefix_level;
+	htable->num_buckets = num_buckets;
+	htable->shamt = shamt;//used by hash function
+	htable->mask = (shamt_mask == 0) ? 0x00000000 : (0xFFFFFFFF << (32-shamt_mask));//used before calling or by insert functions
+	htable->num_entries = 0;
+	htable->num_collisions = 0;
+	htable->buckets = malloc(num_buckets*sizeof(bucket_t*));//moved to array of pointers to buckets, ptrs only populated if index used
+
+	//ptrs only populated if index used
+	for(i=0;i<(num_buckets-1);i++){
+		htable->buckets[i] = NULL;
+	}
+
+	/* not in this function anymore, because buckets is now an array of pointers only, populated if index is used
+	//initialize contents of buckets
+	for(i=0;i<(num_buckets-1);i++){
+		table->buckets[i].nxt_bucket = NULL;
+		table->buckets[i].bucket_type = empty;
+		table->buckets[i].prefix = 0;
+		table->buckets[i].bmp    = 0;
+	}
+	*/
+
+	return table;
+}
+
+uint32_t htable_hash(htable_t * htable, uint32_t key){//uses log2(num_buckets) MSB bits as index into buckets **
+	return (key >> htable->shamt);//made possible by "pre"-calculation on htable creation
+}
+
+bucket_t * htable_search(htable_t * htable, uint32_t prefix){//search for prefix in hash table
+	return htable_search_llist(htable->buckets[htable_hash(htable, prefix)],prefix);
+                                             //^every search need not remember to call hash()
+}
+
+bucket_t * htable_search_llist(bucket_t * bucket, uint32_t prefix){
+	// expects prefix to be masked off corresponding to hash table's level (or precomputed level mask)
+	if(bucket == NULL) return bucket; //return of null means not found/present
+	if(bucket->prefix == prefix) return bucket;//return found matching bucket
+	return htable_search_llist(bucket->nxt_bucket, prefix);//continue to search in collision resolved linked list
+}
+
+void htable_insert(htable_t * htable, uint32_t prefix, bucket_type_t btype){//remember to have already and-ed with mask provided in hash table struct
+	uint32_t index;
+	index = htable_hash(htable, prefix);
+	//increment total entries in this hash table
+	htable->num_entries++;
+
+	//IDEA PASS IN ASSEMBLED BUCKET(which searches if btype is "both") 
+	//or pass all fields of bucket pre-calculated (bucket_type,prefix,bmp,nxt_hop_addr, new_rope)
+	//this function will check for preexisting matching prefixes and update if exist
+
+	in bucket_t bmp and nxt_hop_addr can probably be combined and you know its a bmp for "both or marker" nxt_hop_addr for "both or prefix"
+
+	if(htable->buckets[index] == NULL){//bucket does not exist at this index yet
+		htable->buckets[index] = malloc(sizeof(bucket_t));
+
+	} else {//conflicts, must correctly increment num_collisions
+		
+	}
+}
+
+bucket_t * htable_insert_llist(bucket_t * bucket, uint32_t prefix){
+	//insert at end of list, order doesn't matter as lookups will be random :(
+	if(bucket == NULL){
+		bucket = malloc(sizeof(bucket_t));
+		bucket->
+	}
+}
+
+// ********** END HASHTABLE FUNCTIONS **********
+
+// ********** BEGIN TRIE FUNCTIONS **********
 
 trie_node_t * build_trie_table(route_table_entry_t * table, int num_entries){
 	uint32_t i;
@@ -110,6 +223,7 @@ trie_node_t * insert_trie_node(trie_node_t * trie, route_table_entry_t * table_e
 		trie->mask = (curr_level == 0) ? 0x00000000 : (0xFFFFFFFF << (32-curr_level));//need to be able to shift uint32_t by 32, C/x86 do not support 32 bit shift, only 0-31 bit shift
 		trie->prefix = (table_entry->dest_addr.address & trie->mask);
 		trie->prefixlen = curr_level;//level this node lives at
+		trie->nxt_hop_addr = 0;
 		
 		trie->left = NULL;
 		trie->right= NULL;
@@ -143,6 +257,7 @@ trie_node_t * insert_trie_node(trie_node_t * trie, route_table_entry_t * table_e
 	}
 	else{//my level, mark as real prefix node (not marker)
 		trie->real_prefix = 1;//TRUE
+		trie->nxt_hop_addr = table_entry->next_hop_addr;
 	}
 
 	return trie;
@@ -209,3 +324,7 @@ void destroy_prefix_len_below(prefix_len_below_t * prefix_len_below){
 	free(prefix_len_below);
 	return;
 }
+
+// ********** END TRIE FUNCTIONS **********
+
+
